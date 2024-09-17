e14a40c12c58ae3fdbd63a2eb1b4ed06d03e204f
IT24101125 <IT24101125@my.sliit.lk>
2024-09-17 11:55:31 +0530

    IT24101125Lab6Q2A.java

---
+++ Q2/IT24101125Lab6Q2A.java
@@ -0,0 +1,14 @@
+public class IT24101125Lab6Q2A
+ {
+   public static void main(String[]args)
+    {
+    
+      int number=1;
+
+      while(number<=10)
+      {System.out.print( " " + number + " ");
+      number++;}
+    }
+  } 
+
+      
